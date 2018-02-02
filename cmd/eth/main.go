71cbbb40c471296677a4413d2fd88a721809884c
Philip Hofer <phofer@umich.edu>
2018-02-02 09:49:43 -0800

    fix up cmd listing output

--- cmd/eth/main.go
+++ cmd/eth/main.go
@@ -25,27 +25,27 @@ var subcommands = map[string]*cmd{
 }
 
 func usage() {
-	fmt.Fprintln(os.Stdout, "usage: eth <cmd> <args...>")
-	fmt.Fprintln(os.Stdout, "subcommands:")
-	var out []string
+	fmt.Println("usage: eth <cmd> <args...>")
+	fmt.Println("subcommands:")
+	var out [][2]string
 	for name, c := range subcommands {
-		out = append(out, fmt.Sprintf("\t%s\t\t%s", name, c.desc))
+		out = append(out, [2]string{name, c.desc})
 	}
-	sort.Strings(out)
+	sort.Slice(out, func(i, j int) bool { return out[i][0] < out[j][0] })
 	for i := range out {
-		fmt.Fprintln(os.Stdout, out[i])
+		fmt.Printf("%16s    %s\n", out[i][0], out[i][1])
 	}
 	os.Exit(1)
 }
 
 func main() {
 	args := os.Args
-	if len(args) == 1 {
+	if len(args) == 1 || args[1] == "help" {
 		usage()
 	}
 	cmd, ok := subcommands[args[1]]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "unknown subcommand %q\n", args[0])
+		fmt.Fprintf(os.Stderr, "unknown subcommand %q\n", args[1])
 		usage()
 	}
 	cmd.fs.Parse(args[2:])
